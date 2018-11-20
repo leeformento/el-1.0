78e3ebfb5dc684f777ff7267079eeac4a9e1be91
Lee Len Formento <leebatef@gmail.com>
2018-11-20 12:47:01 -0500

    Watch trailer

--- src/Hero/MainHero.js
+++ src/Hero/MainHero.js
@@ -28,11 +28,34 @@ let BgImage = styled.img`
 `;
 
 let Sofaman = styled.img`
-    margin-top: -50px;
+    margin-top: -14px;
     width: 100%;
     background-size:     cover;                      /* <------ */
     background-repeat:   no-repeat;
     background-position: center center;  
+`;
+
+let DivText = styled.h2`
+      /* position: absolute;  */
+     /* bottom: 8px;
+    left: 16px;  */
+    font-size: 1.2em;
+    text-align: center;
+    color: white;
+    margin-top: -150px;
+    font-family: 'Montserrat';
+
+`;
+let DivP = styled.h2`
+      /* position: absolute;  */
+     /* bottom: 8px;
+    left: 16px;  */
+    font-size: 1em;
+    text-align: center;
+    color: white;
+    margin-top: -10px;
+    font-family: 'Montserrat';
+
 `;
 
 const MainHero = () => {
@@ -42,6 +65,12 @@ const MainHero = () => {
             <BgImage alt="hero" src={require('./assets/bg2.svg')}/>
             {/* <i class="far fa-arrow-alt-circle-down"></i> */}
             <Sofaman alt="hero" src={require('./assets/sofaman.jpg')}/>
+            <Div>
+            <DivText> The new generation of seating. </DivText>
+            </Div>
+            <Div>
+            <DivP> Watch the film <i class="far fa-play-circle"></i> </DivP>
+            </Div>
 
         </Div>
     )
